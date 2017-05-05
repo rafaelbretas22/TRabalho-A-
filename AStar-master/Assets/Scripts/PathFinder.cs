f944130132c9f5d88def4c0a6aabedb29ce7023d
Rafael Bretas <rafael.bretas2000@gmail.com>
2017-05-05 10:44:26 -0300

    Add files via upload

---
+++ AStar-master/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public static class PathFinder
+{
+	public static List<Grid.Position> FindPath( Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition )
+	{
+		var GridPosition = new Grid.Position();
+		var path = new List<Grid.Position>();
+		GridPosition == fromPosition.x;
+		path.Add( fromPosition );
+		path.Add( toPosition);
+		while(GridPosition < toPosition.x)
+		{
+			GridPosition ++;
+		}
+
+
+		return path;
+	}
+}
---
+++ AStar-master/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[System.NonSerialized]
+	public Grid.Position position;
+
+	public void SetPosition( Grid.Position position, Vector2 spacing )
+	{
+		this.position = position;
+		transform.localPosition = position.ToWorldPosition( spacing, 1.0f );
+	}
+}
